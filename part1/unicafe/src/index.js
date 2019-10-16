d05fa49bf96aeaa31a0f466cd1575e58c273f2ca
Or <orshadad@gmail.com>
2019-10-16 21:50:48 +0300

    exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,14 +7,38 @@ const Button = (props) => (
   </button>
 )
 
-const Display = (props) => (
-  <div>
-    {props.text} {props.value}
-  </div>
-)
+const Statistics = (props) => {
+  const totalFunc = () => props.good + props.neutral + props.bad
+  const total = totalFunc()
+
+  return (
+    <div>
+      total - {total}
+      <br></br>
+      average - {((props.good - props.bad) / total) || 0}
+      <br></br>
+      positive - {(props.good / total) || 0} %
+    </div>
+  )
+}
+
+const Display = (props) => {
+
+  if (!props.good && !props.neutral && !props.bad) return 'No feedback given'
+
+  return (
+    <div>
+      good - {props.good}
+      <br></br>
+      neutral - {props.neutral}
+      <br></br>
+      bad - {props.bad}
+      <Statistics good={props.good} neutral={props.neutral} bad={props.bad}></Statistics>
+    </div>
+  )
+}
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -30,9 +54,7 @@ const App = () => {
       <Button handleClick={() => handleClick(neutral + 1, setNeutral)} text='neutral' />
       <Button handleClick={() => handleClick(bad + 1, setBad)} text='bad' />
       <h2>Statistics</h2>
-      <Display value={good} text='good'></Display>
-      <Display value={neutral} text='neutral'></Display>
-      <Display value={bad} text='bad'></Display>
+      <Display good={good} neutral={neutral} bad={bad}></Display>
     </div>
   )
 }
