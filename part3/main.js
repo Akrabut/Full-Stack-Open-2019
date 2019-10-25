3d40dbabd3c886bad320aa3b3cdb03614edb2cc4
Or <orshadad@gmail.com>
2019-10-25 17:57:14 +0300

    3.12

--- part3/main.js
+++ part3/main.js
@@ -36,7 +36,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('build'));
 
-morgan.token('body', req => JSON.stringify(req.body) || '|');
+morgan.token('body', (req) => JSON.stringify(req.body) || '|');
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
