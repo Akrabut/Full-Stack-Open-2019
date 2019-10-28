acaa340fdca01fc69eb0876114518cc42a85270e
Or <orshadad@gmail.com>
2019-10-28 21:37:24 +0200

    up to 4.6

--- part4/test/list_helper.test.js
+++ part4/test/list_helper.test.js
@@ -6,3 +6,54 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+});
+
+describe('highest amount of likes', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    },
+    {
+      title: 'Some weird title',
+      author: 'Some weird guy',
+      likes: 39
+    }
+  ];
+
+  test('blog with highest amount of likes', () => {
+    const result = listHelper.favoriteBlog(listWithBlogs);
+    expect(result).toEqual({
+      title: 'Some weird title',
+      author: 'Some weird guy',
+      likes: 39
+    });
+  });
+});
+
--- part4/utilities/list_helper.js
+++ part4/utilities/list_helper.js
@@ -2,6 +2,22 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  return blogs.reduce((likes, blog) => {
+    return likes + blog.likes;
+  }, 0);
+};
+
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null;
+  return blogs.slice(1, blogs.length).reduce((maxLikes, blog) => {
+    if (blog.likes > maxLikes.likes) maxLikes = blog;
+    return maxLikes;
+  }, blogs[0]);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
